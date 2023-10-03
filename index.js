92435762ac54001659cc167174c89fc30e1fe87d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-03 11:08:49 +0300

    final comit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-console.log("hello john");
-const puppeteer = require('puppeter');
+
+const puppeteer = require('puppeteer');
 
 async function run(){
 const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto('https://www.traversymedia.com');
 
+await page.screenshot({path:"example.png"})
 
+await browser.close();
 }
 
-run
+run();
